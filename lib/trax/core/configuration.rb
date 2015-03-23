4d8aaf30f270c3b083c356f30958e2d1e90e4104
Jason Ayre <jasonayre@gmail.com>
2015-03-22 21:54:46 -0600

    store source class

--- lib/trax/core/configuration.rb
+++ lib/trax/core/configuration.rb
@@ -7,10 +7,12 @@ module Trax
     class Configuration
       include ActiveModel::Validations
 
+      class_attribute :source
+
       def initialize
         set_default_values
 
-        yield self
+        yield self if block_given?
 
         raise_errors unless self.valid?
       end
@@ -22,6 +24,8 @@ module Trax
       end
 
       def self.inherited(subklass)
+        super(subklass)
+
         subklass.class_attribute :configurable_options
         subklass.configurable_options = {}
       end
@@ -31,7 +35,28 @@ module Trax
 
         validates_presence_of(attr_name) if(options.key?(:required))
 
+        if(options.key?(:validates))
+          validates(attr_name, options[:validates])
+        end
+
+        define_method("#{attr_name}=") do |val|
+          value = options.key?(:setter) ? options[:setter].call(val) : val
+          instance_variable_set("@#{attr_name}", value)
+
+          raise_errors unless self.valid?
+        end
+
+        if(options.key?(:getter))
+          define_method(attr_name) do |val|
+            options[:getter].call(val)
+          end
+        end
+
         self.configurable_options[attr_name] = options
+      end
+
+      def merge!(options = {})
+        options.each_pair{ |k,v| __send__("#{k}=", v) }
       end
 
       def raise_errors
--- lib/trax/core/ext/object.rb
+++ lib/trax/core/ext/object.rb
@@ -1,42 +1,47 @@
 require "active_support/core_ext/object/try"
-
+require 'pry'
 class Object
   # Defines a Configuration Class within a target module namespace, or nested class
   # i.e. configuration_for(::Ecommerce::Cart) will define
   # Ecommerce::Cart::Configuration class, inherited form Trax::Core::Configuration
   # And we can then define specific configuration options, for that class, on that class
-  def configuration_for(target, as = :configuration, &blk)
+  def configuration_for(target, as = :configuration, run_configuration = false, &blk)
+    configuration_ivar_name = (as == :configuration) ? :configuration : :"#{as}_configuration"
+    configuration_ivar_name_shortcut = (as == :configuration) ? :config : :"#{as}_config"
+    target.const_set(configuration_ivar_name.to_s.classify, ::Class.new(::Trax::Core::Configuration))
+    configuration_klass = target.const_get(configuration_ivar_name.to_s.classify)
+    configuration_klass.source = target
 
-    target.const_set("Configurations", Class.new) unless const_defined?("Configurations")
-    target.const_get("Configurations").__send__(:attr_accessor, as)
+    configurer_method_name = (as == :configuration) ? :configure : :"configure_#{as}"
+    target.singleton_class.__send__(:attr_accessor, configuration_ivar_name)
+    target.singleton_class.instance_variable_set("@#{configuration_ivar_name}", configuration_klass.instance_eval(&blk))
+    target.singleton_class.__send__(:alias_method, configuration_ivar_name_shortcut, configuration_ivar_name)
 
-    target.class_eval do
-      const_get("Configurations").const_set(as.to_s.classify, ::Class.new(::Trax::Core::Configuration))
-      const_get("Configurations").const_get(as.to_s.classify).instance_eval(&blk)
-
-      unless self.respond_to?(:configure)
-        define_singleton_method(:configure)
-        def self.configure(target_configuration = :configuration, &block)
-          configuration_klass = self::Configurations.const_get(target_configuration)
-          configuration_klass
-
-          instance_variable_set(:"@#{target_configuration}", const_get(target_configuration.classify).new(&block))
-          @_configuration = const_get(target_configuration.classify).new(&block)
-        end
-      end
-
-      def self.config
-        _configuration
+    target.define_singleton_method(configurer_method_name) do |&block|
+      if block
+        return instance_variable_set("@#{configuration_ivar_name}", configuration_klass.new(&block)) if !instance_variable_get("@#{configuration_ivar_name}")
+        return instance_variable_get("@#{configuration_ivar_name}").instance_eval(&block)
+      else
+        return instance_variable_set("@#{configuration_ivar_name}", configuration_klass.new) if !instance_variable_get("@#{configuration_ivar_name}")
+        return instance_variable_get("@#{configuration_ivar_name}")
       end
     end
+
+    target.__send__(configurer_method_name) if run_configuration
   end
 
-  def define_configuration_options(as=:configuration, &block)
-    configuration_for(self, as, &block)
+  def define_configuration_options(as=:configuration, run_configuration = false, &block)
+    configuration_for(self, as, run_configuration, &block)
+  end
+
+  def define_configuration_options!(as=:configuration, &block)
+    define_configuration_options(as, true, &block)
   end
 
   alias_method :define_configuration, :define_configuration_options
   alias_method :has_configuration, :define_configuration_options
+  alias_method :has_configurable_options, :define_configuration_options
+  alias_method :has_configurable_options!, :define_configuration_options!
 
   #following method stolen from abrandoned https://rubygems.org/gems/try_chain
   def try_chain(*symbols)
