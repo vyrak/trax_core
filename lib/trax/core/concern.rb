b0ade76be8430c47d48f06a39db2269761dbbeb5
Jason Ayre <jayre@peer60.com>
2015-05-04 18:14:19 -0600

    fix concern, prototyping alt mixin pattern using just concern

--- lib/trax/core/concern.rb
+++ lib/trax/core/concern.rb
@@ -2,6 +2,11 @@ module Trax
   module Core
     module Concern
       def included(base = nil, &block)
+        unless base.respond_to?(:_mixins_config)
+          base.class_attribute :_mixins_config
+          base._mixins_config = {}
+        end
+
         super(base, &block)
 
         trace = ::TracePoint.new(:end) do |tracepoint|
@@ -10,6 +15,10 @@ module Trax
 
             if self.instance_variable_defined?(:@_after_included_block)
               base.instance_eval(&self.instance_variable_get(:@_after_included_block))
+            end
+
+            if self.instance_variable_defined?(:@_on_mixed_in_block)
+              base.instance_exec(base.mixins_config, &self.instance_variable_get(:@_on_mixed_in_block))
             end
           end
         end
@@ -23,7 +32,7 @@ module Trax
         super(base)
 
         trace = ::TracePoint.new(:end) do |tracepoint|
-          if tracepoint.self == base #modules also trace end we only care about the class end
+          if tracepoint.self == base
             trace.disable
 
             if base.instance_variable_defined?(:@_after_extended_block)
@@ -35,12 +44,10 @@ module Trax
         trace.enable
       end
 
-      def before_extended(&block)
-        self.instance_variable_set(:@_before_extended_block, block)
-      end
-
-      def self.before_extended(&block)
-        self.instance_variable_set(:@_before_extended_block, block)
+      module ClassMethods
+        def configure_mixin(name, options = {})
+          _mixins_config[name] = options
+        end
       end
 
       def after_extended(&block)
@@ -51,16 +58,16 @@ module Trax
         self.instance_variable_set(:@_after_included_block, block)
       end
 
-      def before_included(&block)
-        self.instance_variable_set(:@_before_included_block, block)
-      end
-
       def mixed_in(&block)
         after_included(&block)
       end
 
       def mixed(&block)
         after_included(&block)
+      end
+
+      def on_mixed_in(&block)
+        self.instance_variable_set(:@_on_mixed_in_block, block)
       end
     end
   end
