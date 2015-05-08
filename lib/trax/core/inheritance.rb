7b12c467093365b11f3cc8210c1e8b50facbe029
Jason Ayre <jayre@peer60.com>
2015-05-08 15:24:49 -0600

    inheritance mixin for after_inherited hook to ensure class has been evaluated

--- lib/trax/core/inheritance.rb
+++ lib/trax/core/inheritance.rb
@@ -5,16 +5,29 @@ module Trax
 
       module ClassMethods
         def inherited(subklass)
-          klass = super(subklass)
-          klass.class_attribute(:_after_inherited_block)
+          if self.instance_variable_defined?(:@_on_inherited_block)
+            subklass.instance_eval(&self.instance_variable_get(:@_after_inherited_block))
+          end
 
           trace = ::TracePoint.new(:end) do |tracepoint|
             if tracepoint.self == subklass
               trace.disable
+
+              if self.instance_variable_defined?(:@_after_inherited_block)
+                subklass.instance_eval(&self.instance_variable_get(:@_after_inherited_block))
+              end
             end
           end
 
           trace.enable
+        end
+
+        def after_inherited(&block)
+          self.instance_variable_set(:@_after_inherited_block, block)
+        end
+
+        def on_inherited(&block)
+          self.instance_variable_set(:@_on_inherited_block, block)
         end
       end
     end
