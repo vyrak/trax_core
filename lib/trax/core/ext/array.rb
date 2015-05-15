c2f10a05c30232e7593b6bbf985022f2a335b922
Jason Ayre <jayre@peer60.com>
2015-05-15 12:57:54 -0600

    add support for non hash object when using to proc

--- lib/trax/core/ext/array.rb
+++ lib/trax/core/ext/array.rb
@@ -1,7 +1,15 @@
 class Array
   def to_proc
-    ->(h) { length == 1 ? h[first] : h.values_at(*self) }
+    ->(hash_or_object) {
+      if hash_or_object.is_a?(::Hash)
+        length == 1 ? hash_or_object[first] : hash_or_object.values_at(*self)
+      else
+        length == 1 ? hash_or_object.__send__(first) : self.each_with_object({}){ |method_name,result|
+                                                         result[method_name] = hash_or_object.__send__(method_name)
+                                                       }
+      end
+    }
   end
 end
 
-#credit: http://thepugautomatic.com/2014/11/array-to-proc-for-hash-access/
+#inspired by: http://thepugautomatic.com/2014/11/array-to-proc-for-hash-access/
--- spec/trax/array_spec.rb
+++ spec/trax/array_spec.rb
@@ -12,5 +12,18 @@ describe ::Array do
     it{
       subject.map(&[:name, :price]).map(&:last).sum.should eq 90
     }
+
+    context "array of non hash objects" do
+      subject {
+        [
+          OpenStruct.new({:name => "something", :price => 40}),
+          OpenStruct.new({:name => "else", :price => 50})
+        ]
+      }
+
+      it {
+        subject.map(&[:name, :price]).map(&[:price]).sum.should eq 90
+      }
+    end
   end
 end
