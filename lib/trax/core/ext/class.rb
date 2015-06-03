aa7ccfd2e7e256fddd2889922b23d0b7ddd4d32b
Jason Ayre <jayre@peer60.com>
2015-06-03 07:11:55 -0600

    fix superclasses until method

--- lib/trax/core/ext/class.rb
+++ lib/trax/core/ext/class.rb
@@ -1,24 +1,8 @@
 class Class
-  def self.extend_from_parent_with_hooks(name, parent_klass, &block)
-    trace = ::TracePoint.new(:end) do |tracepoint|
-      if tracepoint.self.class == Class
-        trace.disable
-
-        if parent_klass.instance_variable_defined?(:@_after_inherited_block)
-          self.instance_eval(&parent_klass.instance_variable_get(:@_after_inherited_block))
-        end
-      end
-    end
-
-    trace.enable
-
-    const_set(name, new(parent_klass, &block))
-  end
-
-  def superclasses_until(klass, superclass_chain = [])
-    if superclass != klass
-      superclass_chain << superclass
-      return superclasses_until(superclass, superclass_chain)
+  def superclasses_until(klass, bottom_klass=self, superclass_chain = [])
+    if bottom_klass.superclass != klass
+      superclass_chain.unshift(bottom_klass.superclass)
+      return superclasses_until(klass, bottom_klass.superclass, superclass_chain)
     else
       return superclass_chain
     end
