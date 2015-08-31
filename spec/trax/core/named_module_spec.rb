aeef56193701c34c7cc972eda2709b1ae8a00946
Jason Ayre <jasonayre@gmail.com>
2015-08-31 16:48:00 -0600

    named module spec

---
+++ spec/trax/core/named_module_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe ::Trax::Core::NamedModule do
+  let(:fake_module_name) { "FakeNamespace::Ecommerce" }
+  subject { Object.const_get(fake_module_name) }
+
+  it { expect(subject).to respond_to(:price) }
+  it { expect(subject).to respond_to(:shipping) }
+  it { expect(subject.some_method).to eq "blah" }
+
+  context "includes" do
+    let(:fake_module_name) { "FakeNamespace::ThingWithIncludes" }
+    it { expect(subject::BASE_PRICE).to eq "0.00" }
+  end
+end
