c275bbefe3f5b22cb05dad5e6fa4375cf5ff44f5
Jason Ayre <jasonayre@gmail.com>
2015-08-31 16:47:49 -0600

    named module support

---
+++ spec/support/fake_namespace/named_module_support.rb
@@ -0,0 +1,21 @@
+module PricingExtension
+  BASE_PRICE = "0.00"
+
+  def price
+    "9.99"
+  end
+end
+
+module ShippingExtension
+  def shipping
+    "9.99"
+  end
+end
+
+::Trax::Core::NamedModule.new("FakeNamespace::Ecommerce", PricingExtension, ShippingExtension) do
+  def self.some_method
+    "blah"
+  end
+end
+
+::Trax::Core::NamedModule.new("FakeNamespace::ThingWithIncludes", :includes => [PricingExtension])
