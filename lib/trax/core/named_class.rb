51635bb6607bd36949c300fe5cc309610835df0d
Jason Ayre <jasonayre@gmail.com>
2015-08-31 21:17:45 -0600

    set options after original definition block

--- lib/trax/core/named_class.rb
+++ lib/trax/core/named_class.rb
@@ -4,12 +4,12 @@ module Trax
       def self.new(_name, _parent_klass=nil, **options, &block)
         klass = ::Object.set_fully_qualified_constant(_name, (_parent_klass ? ::Class.new(_parent_klass) : Class.new do
           define_singleton_method(:name) { _name }
-
-          options.each_pair do |k,v|
-            self.class.class_attribute k
-            self.__send__("#{k}=", v)
-          end
         end))
+
+        options.each_pair do |k,v|
+          klass.class_attribute k
+          klass.__send__("#{k}=", v)
+        end unless options.blank?
 
         klass.instance_eval(&block) if block_given?
 
