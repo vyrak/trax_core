a718d5cd34b2940531e7fd9aec7f964182f1104e
Jason Ayre <jasonayre@gmail.com>
2015-09-30 20:44:19 -0600

    add definitions specs

--- spec/trax/core/definitions_spec.rb
+++ spec/trax/core/definitions_spec.rb
@@ -12,6 +12,24 @@ describe ::Trax::Core::Definitions do
 
   context "struct" do
     it { expect(subject::ProductAttributes.new).to be_a(::Trax::Core::Types::Struct) }
+
+    context "float" do
+      it { expect(subject::ProductAttributes.new.price).to eq 9.99 }
+      it { expect(subject::ProductAttributes.new(:price => 9).price).to eq 9.0 }
+    end
+
+    context "integer" do
+      it { expect(subject::ProductAttributes.new.quantity_in_stock).to eq 0 }
+      it { expect(subject::ProductAttributes.new(:quantity_in_stock => 9.19).quantity_in_stock).to eq 9 }
+    end
+
+    context "array" do
+      it { expect(subject::ProductAttributes.new.categories).to be_a(Array) }
+      context "it instantiates members into objects if provided" do
+        it { expect(subject::ProductAttributes.new(:categories => [ :clothing ]).categories[0].to_i).to eq 2 }
+        it { expect(subject::ProductAttributes.new(:categories => [ 2 ]).categories[0].to_s).to eq "clothing" }
+      end
+    end
   end
 
   context "inheritance" do
