d246a2c7de0523cf4c6434ead9b308c69885652a
vyrak bunleang <vyrak.bunleang@gmail.com>
2015-10-12 15:49:32 -0600

    demonstrate nested struck not existing in spec

--- spec/trax/core/types/struct_spec.rb
+++ spec/trax/core/types/struct_spec.rb
@@ -9,6 +9,10 @@ describe ::Trax::Core::Types::Struct do
         string :en, :default => ""
         string :da, :default => ""
         string :ca, :default => "eh"
+
+        struct :territories do
+          string :en, :default => ["US", "GB"]
+        end
       end
     end
   end
@@ -19,6 +23,7 @@ describe ::Trax::Core::Types::Struct do
   it { expect(subject.en).to eq "something" }
   it { expect(subject.da).to eq "" }
   it { expect(subject.ca).to eq "eh" }
+  it { expect(subject.territories.en).to eq ["US", "GB"] }
 
   context "unknown value" do
     subject { "::MyFakeStructNamespace::Locale".constantize.new(:blah => "something") }
