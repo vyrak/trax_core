47f7f4563c82327941bcf0d3b6e975501682d0de
Jason Ayre <jasonayre@gmail.com>
2016-03-11 18:41:35 -0700

    blow up if dig shim not required

--- lib/trax/core.rb
+++ lib/trax/core.rb
@@ -14,6 +14,17 @@ require_relative './core/ext/uri'
 require_relative './core/primitives/enum_value'
 require_relative './core/primitives/enum'
 
+if(::RUBY_VERSION < '2.3')
+  begin
+    require 'ruby_dig'
+  rescue LoadError => e
+    raise unless e.message =~ /ruby_dig/
+    error = e.exception("Trax::Core::Transformer relies on ruby_dig shim, add gem 'ruby_dig' to gemfile")
+    error.set_backtrace(e.backtrace)
+    raise error
+  end
+end
+
 module Trax
   module Core
     extend ::ActiveSupport::Autoload
