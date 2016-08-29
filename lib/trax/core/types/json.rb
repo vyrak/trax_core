17cf3da0fce28b3f78b023eb7f900c5c4b17c669
Jason Ayre <jayre@peer60.com>
2016-08-29 13:56:16 -0600

    add to schema method for json value object

--- lib/trax/core/types/json.rb
+++ lib/trax/core/types/json.rb
@@ -5,6 +5,14 @@ module Trax
         def self.type
           :json
         end
+
+        def self.to_schema
+          ::Trax::Core::Definition.new(
+            :name => self.name.demodulize.underscore,
+            :source => self.name,
+            :type => self.type
+          )
+        end
       end
     end
   end
